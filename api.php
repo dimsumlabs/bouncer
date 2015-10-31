337198ddfaf3ca1fb1310c99a746265d23b14f81
Lionello Lunesu <lio+git@lunesu.com>
2015-10-31 21:19:43 +0800

    Added RFID support to the API: api.php?rfid=HASH

--- api.php
+++ api.php
@@ -2,19 +2,28 @@
 require 'inc/common.php';
 require 'inc/db.php';
 
-$password = $_GET['password'];
-$email = trim($_GET['email']);
-
-$salt = 'salT';
-$passwordx = sprintf("%08x", crc32($salt.strtoupper($email)));
-
-$email2 = $link->escapeString($email);
-$password2 = $link->escapeString($password);
-$link->exec("UPDATE Users SET count = count + 1 WHERE DATE('now') <= paid AND email = '$email2' AND password = '$password2'")
-	or die('link->exec UPDATE error');
-if ($link->changes() != 1)
-	header('HTTP/1.1 403 Forbidden');
+$rfid = $_GET['rfid'];
+if ($rfid) {
+  $rfid2 = $link->escapeString($rfid);
+  $link->exec("UPDATE Users SET count = count + 1 WHERE DATE('now') <= paid AND rfid = '$rfid2'")
+          or die('link->exec UPDATE error');
+}
 else
-	header('HTTP/1.1 200 OK');
+{
+  $password = $_GET['password'];
+  $email = trim($_GET['email']);
+
+  $salt = 'salT';
+  $passwordx = sprintf("%08x", crc32($salt.strtoupper($email)));
+
+  $email2 = $link->escapeString($email);
+  $password2 = $link->escapeString($password);
+  $link->exec("UPDATE Users SET count = count + 1 WHERE DATE('now') <= paid AND email = '$email2' AND password = '$password2'")
+          or die('link->exec UPDATE error');
+}
+if ($link->changes() != 1)
+        header('HTTP/1.1 403 Forbidden');
+else
+        header('HTTP/1.1 200 OK');
 $link->close();
 unset($link);
