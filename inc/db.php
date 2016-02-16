e6be6f4db5755e1dfededbf6bf41d145e7497177
Lionello Lunesu <lio+git@lunesu.com>
2016-02-16 17:37:03 +0800

    Skip ALTER TABLE

--- inc/db.php
+++ inc/db.php
@@ -4,5 +4,5 @@ require_once 'mailer.php';
 $link = new SQLite3('/var/bouncer/members.db')
   or mail_and_die('SQLite3 ctor error', __FILE__);
 
-$link->exec('ALTER TABLE Users ADD COLUMN `name` char(64) NULL');
+//$link->exec('ALTER TABLE Users ADD COLUMN `name` char(64) NULL');
 
