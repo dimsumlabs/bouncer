dc1a4fafd1bc06f060a8854f38ca523fcd1257d0
Lionello Lunesu <lio+git@lunesu.com>
2016-05-01 16:33:57 +0800

    Added Groups table and group auth api for permissions

--- api.php
+++ api.php
@@ -2,11 +2,21 @@
 require 'inc/common.php';
 require 'inc/db.php';
 
+// TODO: use a shared secret + hash for API authentication
+
 $rfid = $_GET['rfid'];
 if ($rfid) {
   $rfid2 = $link->escapeString($rfid);
-  $link->exec("UPDATE Users SET count = count + 1, last_seen = DATETIME('now') WHERE DATE('now') <= MAX(IFNULL(paid,0),IFNULL(paid_verified,0)) AND rfid = '$rfid2'")
-          or die('link->exec UPDATE error');
+  $group = trim($_GET['group']);
+  if ($group) {
+    $group2 = $link->escapeString($group);
+    $link->exec("UPDATE Groups SET count = count + 1 WHERE \"group\" = '$group2' AND email = (SELECT email FROM Users WHERE DATE('now') <= MAX(IFNULL(paid,0),IFNULL(paid_verified,0)) AND rfid = '$rfid2')")
+      or die('link->exec UPDATE error(0)');
+  }
+  else {
+    $link->exec("UPDATE Users SET count = count + 1, last_seen = DATETIME('now') WHERE DATE('now') <= MAX(IFNULL(paid,0),IFNULL(paid_verified,0)) AND rfid = '$rfid2'")
+      or die('link->exec UPDATE error(1)');
+  }
 }
 else
 {
@@ -19,11 +29,14 @@ else
   $email2 = $link->escapeString($email);
   $password2 = $link->escapeString($password);
   $link->exec("UPDATE Users SET count = count + 1, last_seen = DATETIME('now') WHERE DATE('now') <= MAX(IFNULL(paid,0),IFNULL(paid_verified,0)) AND email = '$email2' AND password = '$password2'")
-          or die('link->exec UPDATE error');
+    or die('link->exec UPDATE error(2)');
 }
-if ($link->changes() != 1)
-        header('HTTP/1.1 403 Forbidden');
-else
-        header('HTTP/1.1 200 OK');
+
+if ($link->changes() != 1) {
+  header('HTTP/1.1 403 Forbidden');
+}
+else {
+  header('HTTP/1.1 200 OK');
+}
 $link->close();
 unset($link);
--- db.sql
+++ db.sql
@@ -1,48 +1,58 @@
---CREATE DATABASE `members`;
---USE `members`;
+--CREATE DATABASE "members";
+--USE "members";
 
-CREATE TABLE `Payments` (
-  `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `email` char(64) NOT NULL COLLATE NOCASE,
-  `submitted` datetime NOT NULL,
-  `amount` int(11) NOT NULL,
-  `verified` tinyint(1) DEFAULT NULL
+CREATE TABLE "Payments" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "email" varchar(64) NOT NULL COLLATE NOCASE,
+  "submitted" datetime NOT NULL,
+  "amount" int(11) NOT NULL,
+  "verified" tinyint(1) DEFAULT NULL
 );
-DROP INDEX IF EXISTS `IDX_Payments_EMAIL`;
-CREATE INDEX `IDX_Payments_EMAIL` ON `Payments`(`email`);
+DROP INDEX IF EXISTS "IDX_Payments_EMAIL";
+CREATE INDEX "IDX_Payments_EMAIL" ON "Payments"("email");
 
---ALTER TABLE `Users` RENAME TO `Users_old`;
-CREATE TABLE `Users` (
-  `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `email` char(64) NOT NULL COLLATE NOCASE,
-  `password` char(32) DEFAULT NULL,
-  `salt` char(32) DEFAULT NULL,
-  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_seen` datetime DEFAULT NULL,
-  `paid_verified` date DEFAULT NULL,
-  `paid` date DEFAULT NULL,
-  `since` date NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `count` int(11) NOT NULL DEFAULT 0,
-  `mac` char(40) DEFAULT NULL COLLATE NOCASE,
-  `rfid` char(64) DEFAULT NULL COLLATE NOCASE,
-  `name` char(64) DEFAULT NULL COLLATE NOCASE
+--ALTER TABLE "Users" RENAME TO "Users_old";
+CREATE TABLE "Users" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "email" varchar(64) NOT NULL COLLATE NOCASE,
+  "password" varchar(32) DEFAULT NULL,
+  "salt" varchar(32) DEFAULT NULL,
+  "last_update" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "last_seen" datetime DEFAULT NULL,
+  "paid_verified" date DEFAULT NULL,
+  "paid" date DEFAULT NULL,
+  "since" date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "count" int(11) NOT NULL DEFAULT 0,
+  "mac" char(40) DEFAULT NULL COLLATE NOCASE,
+  "rfid" char(64) DEFAULT NULL COLLATE NOCASE,
+  "name" varchar(64) DEFAULT NULL COLLATE NOCASE
 );
---INSERT INTO `Users` SELECT * FROM `Users_old`;
---DROP TABLE `Users_old`;
-DROP INDEX IF EXISTS `IDX_Users_EMAIL`;
-CREATE UNIQUE INDEX `IDX_Users_EMAIL` ON `Users`(`email`);
-DROP INDEX IF EXISTS `IDX_Users_PASSWORD`;
-CREATE UNIQUE INDEX `IDX_Users_PASSWORD` ON `Users`(`password`);
-DROP INDEX IF EXISTS `IDX_Users_MAC`;
-CREATE UNIQUE INDEX `IDX_Users_MAC` ON `Users`(`mac`);
-DROP INDEX IF EXISTS `IDX_Users_RFID`;
-CREATE UNIQUE INDEX `IDX_Users_RFID` ON `Users`(`rfid`);
+--INSERT INTO "Users" SELECT * FROM "Users_old";
+--DROP TABLE "Users_old";
+DROP INDEX IF EXISTS "IDX_Users_EMAIL";
+CREATE UNIQUE INDEX "IDX_Users_EMAIL" ON "Users"("email");
+DROP INDEX IF EXISTS "IDX_Users_PASSWORD";
+CREATE UNIQUE INDEX "IDX_Users_PASSWORD" ON "Users"("password");
+DROP INDEX IF EXISTS "IDX_Users_MAC";
+CREATE UNIQUE INDEX "IDX_Users_MAC" ON "Users"("mac");
+DROP INDEX IF EXISTS "IDX_Users_RFID";
+CREATE UNIQUE INDEX "IDX_Users_RFID" ON "Users"("rfid");
 
-CREATE TRIGGER IF EXISTS [UpdateLastTime];
-CREATE TRIGGER [UpdateLastTime]
+CREATE TRIGGER IF EXISTS "UpdateLastTime";
+CREATE TRIGGER "UpdateLastTime"
   AFTER UPDATE
   ON Users
   FOR EACH ROW
   BEGIN
     UPDATE Users SET last_update = CURRENT_TIMESTAMP WHERE id = old.id;
   END;
+
+CREATE TABLE "Groups" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "email" varchar(64) NOT NULL COLLATE NOCASE,
+  "group" varchar(32) NOT NULL COLLATE NOCASE,
+  "count" int(11) NOT NULL DEFAULT 0
+);
+DROP INDEX IF EXISTS "IDX_Groups_EMAIL_GROUP";
+CREATE UNIQUE INDEX "IDX_Groups_EMAIL_GROUP" ON "Groups"("email","group");
+
