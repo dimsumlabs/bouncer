431d78a3ed3cffc510d7f02d59b509532b699a99
Lionello Lunesu <lio+git@lunesu.com>
2016-05-07 14:06:52 +0800

    If Update mac failed, try without mac

--- api/open.php
+++ api/open.php
@@ -20,8 +20,9 @@ else
         $rfid2 = '';
 
 // Give new members the benefit of the doubt (trust, but verify):
-$query = 'UPDATE Users SET count = count + 1'.$mac2.$rfid2.", last_seen = DATETIME('now') WHERE DATE('now') <= MAX(IFNULL(paid,0),IFNULL(paid_verified,0)) AND password = '$password2'";
+$query = "UPDATE Users SET count = count + 1$mac2$rfid2, last_seen = DATETIME('now') WHERE DATE('now') <= MAX(IFNULL(paid,0),IFNULL(paid_verified,0)) AND password = '$password2'";
 $link->exec($query)
+    or $link->exec("UPDATE Users SET count = count + 1$rfid2, last_seen = DATETIME('now') WHERE DATE('now') <= MAX(IFNULL(paid,0),IFNULL(paid_verified,0)) AND password = '$password2'")
 	or mail_and_die('link->exec UPDATE error', $query."\n\n-- ".__FILE__);
 
 if ($link->changes() != 1)
